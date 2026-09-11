a5fe912c6fdffc44bead557ebbd899956f955325
Suraj Sharma <156873770+Suraj8Sharma@users.noreply.github.com>
2026-09-11 15:49:23 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+Node *dfs(Node *node,unordered_map<Node*,Node*>&mp){
+    vector<Node*>neighbours;
+    Node *clone=new Node(node->val);
+    mp[node]=clone;
+    for(auto i :node->neighbors){
+        if(mp.contains(i)){
+            neighbours.push_back(mp[i]);
+        }else{
+            neighbours.push_back(dfs(i,mp));
+        }
+    }
+    clone->neighbors=neighbours;
+    return clone;
+}
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*>mp;
+        if(node==nullptr){
+            return nullptr;
+        }
+        if(node->neighbors.size()==0){
+            Node * clone=new Node(node->val);
+            return clone;
+        }
+
+        //otherwise i need to make a funnction which do the above task
+        return dfs(node,mp);
+
+
+    }
+};
